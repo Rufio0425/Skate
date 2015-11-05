197a11f7cf00c74a3d9492c4e98c4ae09729b7c7
Sean <seanpeter.aguirre@gmail.com>
2015-11-04 23:44:48 -0700

    volunteer details js merge

--- Skate/public/main.js
+++ Skate/public/main.js
@@ -1,3 +1,48 @@
 $(function(){
-    
+    function addEditor(){
+        $('.editors').on('click', function(){
+            $(".bio_input").val($('.bio .val').text());
+            $(".bio .txtarea").show();
+            $(".bio .editors").hide();
+            $(".bio .val").hide();
+        });
+    }
+
+    addEditor();
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': '{!! csrf_token() !!}'
+        }
+    });
+// SAVE
+    $('.bio').on('click', '.save', function(){
+        console.log('Hello');
+        var updatedBio =  $('.bio_input').val()
+        var formData = {
+            bio: updatedBio,
+        };
+        var button = this;
+
+        $.post( "/api/volunteer/{{ $volunteer->id }}/update", formData, function( data ) {
+            $(".bio .txtarea").hide();
+            $(".bio .val").html(updatedBio);
+            $(".bio .editors").show();
+            $(".bio .val").show();
+        }); 
+    });
+// CANCEL
+    $('.bio').on('click', '.cancel', function(){
+        $(".bio .txtarea").hide();
+        $(".bio .editors").show();
+        $(".bio .val").show();
+    });
+
+    $('.slider').slick({
+        dots: true,
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplaySpeed: 2000,
+    });
 });
